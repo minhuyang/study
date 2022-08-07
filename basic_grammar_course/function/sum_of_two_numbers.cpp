51bd28ecb8951abfef5686139fa03f391d9d276d
minhu yang <1552035834@qq.com>
2022-08-07 19:01:35 +0800

    add sum_of_two_numbers

---
+++ basic_grammar_course/function/sum_of_two_numbers.cpp
@@ -0,0 +1,31 @@
+/*
+AcWing 806. 两个数的和
+
+输入两个浮点数 x 和 y，请你编写一个函数，double add(double x, double y)，计算并输出 x 与 y 的和。
+
+输入格式
+共一行，包含两个浮点数 x 和 y。
+
+输出格式
+共一行，包含一个浮点数，表示两个数的和，结果保留 2 位小数。
+
+数据范围
+−1000≤x,y≤1000
+输入样例：
+1.11 2.22
+输出样例：
+3.33
+*/
+#include <iostream>
+using namespace std;
+double add(double x, double y)
+{
+    return x+y;
+}
+int main()
+{
+    double x,y;
+    cin>>x>>y;
+    printf("%.2f\n",add(x,y));
+    return 0;
+}
